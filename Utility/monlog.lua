b87edbd980198a0d4affda63a0bc79309aa911bf
Yue-bin <3103174284@qq.com>
2024-06-30 11:29:43 +0800

    fix: -修复了dellog函数输入为0或负数时会把你的日志全扬了的问题 -新增了文件锁和日志队列，使dellog变为原子操作

--- Utility/monlog.lua
+++ Utility/monlog.lua
@@ -1,29 +1,53 @@
+--日志路径,默认为当前目录下的log.log,可自行修改
+local logpath = "./log.log"
+--日志读取器和日志添加器,默认为nil
+local logreader = nil
+local logadder = nil
+--mua的不加锁不行了
+local filelock = false
+--日志队列，用于删除日志时暂存
+local queue = {}
+
 --小小的日志功能
 monlog = {
 
     --@region private
-    --日志路径,默认为当前目录下的log.log,可自行修改
-    logpath = "./log.log",
-    --日志读取器和日志添加器,默认为nil
-    logreader = nil,
-    logadder = nil,
+
     --@endregion
 
     --@region public
     --使用日志路径初始化,返回是否成功
     init = function(path)
-        monlog.logpath = path or monlog.logpath
-        monlog.logadder = io.open(monlog.logpath, "a")
-        monlog.logreader = io.open(monlog.logpath, "r")
-        if monlog.logadder == nil or monlog.logreader == nil then
+        logpath = path or logpath
+        logadder = io.open(logpath, "a")
+        logreader = io.open(logpath, "r")
+        if logadder == nil or logreader == nil then
             return false
         end
+        filelock = false
         return true
     end,
 
-    --记录到日志中
+    --记录到日志(流)中
     log = function(msg)
-        monlog.logadder:write(os.date("%Y.%m.%d-%H:%M:%S  ") .. msg .. "\n")
+        if logadder == nil then
+            return false
+        end
+        if filelock == true then
+            table.insert(queue, msg)
+            return false
+        end
+        logadder:write(os.date("%Y.%m.%d-%H:%M:%S  ") .. msg .. "\n")
+        return true
+    end,
+
+    --刷新日志流
+    flush = function()
+        if logadder == nil then
+            return false
+        end
+        logadder:flush()
+        return true
     end,
 
     --时间字符串转时间戳
@@ -36,7 +60,10 @@ monlog = {
 
     --获取一个日志迭代器
     getlogiterator = function()
-        return monlog.logreader:lines()
+        if logreader == nil then
+            return nil
+        end
+        return logreader:lines()
     end,
 
     --接受一行日志,返回日志中的时间字符串
@@ -64,29 +91,44 @@ monlog = {
 
     --删除日志前n行
     dellog = function(n)
+        if logreader == nil then
+            return false
+        end
         n = n or 0
-        monlog.logreader:seek("set", 0)
+        if n <= 0 then
+            return false
+        end
+        filelock = true
+        logreader:seek("set", 0)
         local tmpline = ""
         repeat
-            tmpline = monlog.logreader:read()
+            tmpline = logreader:read()
             n = n - 1
         until n == 0 or tmpline == nil
         --其实这里不用判空lua他自己会停下来的，但是我素质比较高（
-        local content = monlog.logreader:read("*a")
+        local content = logreader:read("*a")
         monlog.close()
-        local tmpadder = io.open(monlog.logpath, "w")
+        local tmpadder = io.open(logpath, "w")
         if tmpadder == nil then
             return false
         end
         tmpadder:write(content)
         tmpadder:close()
-        monlog.init(monlog.logpath)
+        monlog.init(logpath)
+        --处理队列
+        for _, v in ipairs(queue) do
+            monlog.log(v)
+        end
         return true
     end,
     --关闭日志
     close = function()
-        monlog.logadder:close()
-        monlog.logreader:close()
+        if logadder ~= nil then
+            logadder:close()
+        end
+        if logreader ~= nil then
+            logreader:close()
+        end
     end
     --@endregion
 }
