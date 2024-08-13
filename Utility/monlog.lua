3d003af4c4ef5e530862d2e22de1a16eb8576997
Yue-bin <3103174284@qq.com>
2024-08-13 21:02:53 +0800

    使用模块的方式，将日志模块化，方便调用 我草你妈啊我是傻逼 手动忽略前两条commit，因为我用了素质非常低的手动版本管理

--- Utility/monlog.lua
+++ Utility/monlog.lua
@@ -1,55 +1,60 @@
+--monlog.lua
+--小小的日志功能
+monlog = {}
+
+
+--@region private
+
 --日志路径,默认为当前目录下的log.log,可自行修改
 local logpath = "./log.log"
+
 --日志读取器和日志添加器,默认为nil
 local logreader = nil
 local logadder = nil
+
 --mua的不加锁不行了
 local filelock = false
+
 --日志队列，用于删除日志时暂存
 local queue = {}
 
---小小的日志功能
+--@endregion
 
-monlog = {}
 
-    --@region private
+--@region public
+--使用日志路径初始化,返回是否成功
+function monlog.init(path)
+    logpath = path or logpath
+    logadder = io.open(logpath, "a")
+    logreader = io.open(logpath, "r")
+    if logadder == nil or logreader == nil then
+        return false
+    end
+    filelock = false
+    return true
+end
 
-    --@endregion
+--记录到日志(流)中
+function monlog.log(msg)
+    if logadder == nil then
+        return false
+    end
+    if filelock == true then
+        table.insert(queue, msg)
+        return false
+    end
+    logadder:write(os.date("%Y.%m.%d-%H:%M:%S  ") .. msg .. "\n")
+    return true
+end
 
-    --@region public
-    --使用日志路径初始化,返回是否成功
-    init = function(path)
-        logpath = path or logpath
-        logadder = io.open(logpath, "a")
-        logreader = io.open(logpath, "r")
-        if logadder == nil or logreader == nil then
-            return false
-        end
-        filelock = false
-        return true
-    end,
-
-    --记录到日志(流)中
-    log = function(msg)
-        if logadder == nil then
-            return false
-        end
-        if filelock == true then
-            table.insert(queue, msg)
-            return false
-        end
-        logadder:write(os.date("%Y.%m.%d-%H:%M:%S  ") .. msg .. "\n")
-        return true
-    end,
-
-    --刷新日志流
-    flush = function()
-        if logadder == nil then
-            return false
-        end
-        logadder:flush()
-        return true
-    end,
+--刷新日志流
+function monlog.flush()
+    if logadder == nil then
+        return false
+    end
+    logadder:flush()
+    return true
+end
 
 --时间字符串转时间戳
 function monlog.timestr2Stamp(timestr)
@@ -59,13 +64,13 @@ function monlog.timestr2Stamp(timestr)
     return timestamp
 end
 
-    --获取一个日志迭代器
-    getlogiterator = function()
-        if logreader == nil then
-            return nil
-        end
-        return logreader:lines()
-    end,
+--获取一个日志迭代器
+function monlog.getlogiterator()
+    if logreader == nil then
+        return nil
+    end
+    return logreader:lines()
+end
 
 --接受一行日志,返回日志中的时间字符串
 function monlog.getlogdate(logstr)
@@ -90,46 +95,49 @@ function monlog.getlogover(sec)
     return count
 end
 
-    --删除日志前n行
-    dellog = function(n)
-        if logreader == nil then
-            return false
-        end
-        n = n or 0
-        if n <= 0 then
-            return false
-        end
-        filelock = true
-        logreader:seek("set", 0)
-        local tmpline = ""
-        repeat
-            tmpline = logreader:read()
-            n = n - 1
-        until n == 0 or tmpline == nil
-        --其实这里不用判空lua他自己会停下来的，但是我素质比较高（
-        local content = logreader:read("*a")
-        monlog.close()
-        local tmpadder = io.open(logpath, "w")
-        if tmpadder == nil then
-            return false
-        end
-        tmpadder:write(content)
-        tmpadder:close()
-        monlog.init(logpath)
-        --处理队列
-        for _, v in ipairs(queue) do
-            monlog.log(v)
-        end
-        return true
-    end,
-    --关闭日志
-    close = function()
-        if logadder ~= nil then
-            logadder:close()
-        end
-        if logreader ~= nil then
-            logreader:close()
-        end
+--删除日志前n行
+function monlog.dellog(n)
+    if logreader == nil then
+        return false
     end
-    --@endregion
-}
+    n = n or 0
+    if n <= 0 then
+        return false
+    end
+    filelock = true
+    logreader:seek("set", 0)
+    local tmpline = ""
+    repeat
+        tmpline = logreader:read()
+        n = n - 1
+    until n == 0 or tmpline == nil
+    --其实这里不用判空lua他自己会停下来的，但是我素质比较高（
+    local content = logreader:read("*a")
+    monlog.close()
+    local tmpadder = io.open(logpath, "w")
+    if tmpadder == nil then
+        return false
+    end
+    tmpadder:write(content)
+    tmpadder:close()
+    monlog.init(logpath)
+    --处理队列
+    for _, v in ipairs(queue) do
+        monlog.log(v)
+    end
+    return true
+end
+
+--关闭日志
+function monlog.close()
+    if logadder ~= nil then
+        logadder:close()
+    end
+    if logreader ~= nil then
+        logreader:close()
+    end
+end
+
+--@endregion
+
+return monlog
