0ffb2ad2cfa9540e84b39b8a6c51f146bc08ab03
Yue-bin <3103174284@qq.com>
2024-08-17 04:09:14 +0800

    增加了几个函数，放宽了json的检测

--- MonUtility.lua
+++ MonUtility.lua
@@ -37,3 +37,23 @@ function Trim_Space_HeadEnd(str)
     local trimmed_str = str:gsub("^%s*(.-)%s*$", "%1")
     return trimmed_str
 end
+
+function TrimTrailingNewlines(str)
+    -- 使用gsub移除末尾的换行符
+    return str:gsub("%s*$", ""):gsub("\r?\n$", "")
+end
+
+function TrimAfterLastBracket(str)
+    -- 查找最后一个 '}' 的位置
+    local lastBracketPos = str:reverse():find("}", 1, true)
+
+    -- 如果找到了 '}', 则从字符串中删除它之后的所有字符
+    if lastBracketPos then
+        lastBracketPos = #str + 1 - lastBracketPos
+        -- 我们需要保留 '}'，所以这里使用 sub(1, lastBracketPos + 1)
+        return str:sub(1, lastBracketPos + 1)
+    else
+        -- 如果没有找到 '}', 返回原字符串
+        return str
+    end
+end
